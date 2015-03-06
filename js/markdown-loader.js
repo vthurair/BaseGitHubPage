b56309201bedfb40d24194b0372ad40c28b705b6
b3ll <adam@adambell.me>
2015-03-06 16:16:42 -0500

    Make md loading more dynamic

--- js/markdown-loader.js
+++ js/markdown-loader.js
@@ -2,7 +2,12 @@
 //  markdown-loader.h
 //  Base GitHub.io Page
 //
-//  This file loads whatever's in page.md (outside this folder), and spits it into the DOM using markdown-js.
+//  This file loads whatever's specified by the div's attribute markdown-file (outside this folder, index.html), and spits it into the DOM using markdown-js.
+//
+//  eg.
+//
+//  <div id="markdown" markdown-file="page.md"> <--- whatever file you want
+//  </div>
 //
 //  Created by Adam Bell on 2015-03-06.
 //  Copyright (c) 2015 Adam Bell. All rights reserved.
@@ -10,13 +15,16 @@
 
 window.addEventListener('load', function() {
 
-var http_client = new XMLHttpRequest();
-http_client.open('GET', '../page.md');
-http_client.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		document.getElementById("markdown").innerHTML = markdown.toHTML(this.response);
+	var markdown_div = document.getElementById('markdown');
+  markdown_filename = markdown_div.getAttribute('markdown-file');
+
+	var http_client = new XMLHttpRequest();
+	http_client.open('GET', '../' + markdown_filename);
+	http_client.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			markdown_div.innerHTML = markdown.toHTML(this.response);
+		}
 	}
-}
-http_client.send();
+	http_client.send();
 
 }, false);
