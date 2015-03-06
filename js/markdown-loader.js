323bbc68c25f71602558ccad58503289e71a0385
b3ll <adam@adambell.me>
2015-03-06 01:37:54 -0500

    Added cool stuff

---
+++ js/markdown-loader.js
@@ -0,0 +1,22 @@
+//
+//  markdown-loader.h
+//  Base GitHub.io Page
+//
+//  This file loads whatever's in page.md (outside this folder), and spits it into the DOM using markdown-js.
+//
+//  Created by Adam Bell on 2015-03-06.
+//  Copyright (c) 2015 Adam Bell. All rights reserved.
+//
+
+window.addEventListener('load', function() {
+
+var http_client = new XMLHttpRequest();
+http_client.open('GET', '../page.md');
+http_client.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		document.getElementById("markdown").innerHTML = markdown.toHTML(this.response);
+	}
+}
+http_client.send();
+
+}, false);
